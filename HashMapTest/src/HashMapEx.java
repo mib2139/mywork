f8b0c77a1a7a8751a869a3554130c76d11582fb0
user42 <user42@gntp-42.gntp.local>
2018-04-27 16:48:13 +0900

    plus straw

--- HashMapTest/src/HashMapEx.java
+++ HashMapTest/src/HashMapEx.java
@@ -4,30 +4,29 @@ import java.util.Scanner;
 public class HashMapEx {
 	public static void main(String[] args) {
 		HashMap<String, String> dic = new HashMap<String, String>();
-		
+
 		dic.put("baby", "아가");
 		dic.put("love", "사랑");
 		dic.put("apple", "사과");
-		
+		dic.put("strawberry","딸기");
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		while (true) {
 			System.out.println("찾고싶은 단어는?");
 			String str = sc.nextLine();
 			if (str.equals("exit")) {
 				System.out.println("종료합니당");
 				break;
-			
 			}
+			
 			String kor = dic.get(str);
+			
 			if (kor==null) {
 				System.out.println(str+"은 없는 단어입니다");
-				
 			}else {
 				System.out.println(kor);
 			}
-			
 		}
-
 	}
 }
